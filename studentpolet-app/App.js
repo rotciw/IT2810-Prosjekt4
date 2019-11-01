49a67d34042cd3273e90efc3e2d0964aa06ec06b
MattisTrygstad <mattislt@sf-nett.no>
2019-11-01 17:42:51 +0100

    initialized project #1

---
+++ studentpolet-app/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({ uri: 'http://it2810-38.idi.ntnu.no:3000/graphql' });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello world!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
