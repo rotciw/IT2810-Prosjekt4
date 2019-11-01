4618598e0b4e478252f6f3c2374d58242aaa8c24
MattisTrygstad <mattislt@sf-nett.no>
2019-11-01 18:38:28 +0100

    added header component #1

---
+++ studentpolet-app/components/header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+} from 'react-native';
+
+export default function Header(props) {
+  return (
+    <Text style={styles.headerText}>Studentpolet</Text>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerText: {
+    fontSize: 25,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+});
