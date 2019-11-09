e2eb000c440c1f53d19741ea5f2e29a9911151ed
Wictor <wictor_x@hotmail.com>
2019-11-09 16:39:35 +0100

    searchbar styling #16#

--- studentpolet-app/src/styles/searchBar.js
+++ studentpolet-app/src/styles/searchBar.js
@@ -3,9 +3,10 @@ import {
 } from 'react-native'
 
 export const styles = StyleSheet.create({
-
     container: {
         backgroundColor: '#2D2D2D',
-        padding:15,
-    }
+        borderBottomColor: 'transparent',
+        borderTopColor: 'transparent',
+        padding: 3
+    },
 })
