8a88a1dc6386e5890a8718040fe1bfa992af7e90
Wictor <wictor_x@hotmail.com>
2019-11-11 17:33:23 +0100

    Hotfix colors

--- studentpolet-app/src/navigation/MainTabNavigator.js
+++ studentpolet-app/src/navigation/MainTabNavigator.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Platform } from 'react-native';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 
-import TabBarIcon from '../components/TabBarIcon';
+import TabBarIcon from './TabBarIcon/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 import FavoriteScreen from '../screens/FavoriteScreen';
 
