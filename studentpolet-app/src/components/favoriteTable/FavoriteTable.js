688f8362a29ded936584083a7256229d6f5f202e
Kasper Maagerø Svendsen <kasperms@stud.ntnu.no>
2019-11-13 12:10:50 +0100

    Added delete button to favorite page

--- studentpolet-app/src/components/favoriteTable/FavoriteTable.js
+++ studentpolet-app/src/components/favoriteTable/FavoriteTable.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { View, FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
-import {AsyncStorage, Linking} from 'react-native';
+import {AsyncStorage, Linking, StyleSheet} from 'react-native';
 import {NavigationEvents} from 'react-navigation';
+import Swipeout from 'react-native-swipeout';
+
 
 //List of favorites
 class FavoriteTable extends Component{
@@ -10,7 +12,15 @@ class FavoriteTable extends Component{
         super(props);
         this.state = {
           data: null,
+          currItem: null
         }
+        this.swipeoutBtns = [
+          {
+            text: 'Delete',
+            backgroundColor: 'red',
+            onPress: () => this.removeFavorite(this.state.currItem)
+          }
+        ]
     }
 
     keyExtractor = (item, index) => index.toString()
@@ -25,17 +35,43 @@ class FavoriteTable extends Component{
       }
     }
 
+    removeFavorite = async (favorite) => {
+      try {
+          let data = await AsyncStorage.getItem('Favorites') || [];
+          if (typeof data === 'string'){
+            data = JSON.parse(data);
+          }
+          for(let i = data.length - 1; i >= 0; i--) {
+              if(data[i].Varenummer == favorite.Varenummer) {
+                  //Fjerner data om det allerede finnes
+                 data.splice(i, 1);
+              }
+          }
+          await AsyncStorage.removeItem('Favorites');
+          await AsyncStorage.setItem('Favorites', JSON.stringify(data));
+        } catch (error) {
+          // Error saving data
+        }
+        this.getData();
+      };
+
     //The view of every item, same as in Table
     renderItem = ({ item }) => (
-      <ListItem
-          title={item.Varenavn}
-          leftAvatar={{ height: 64, width: 32, resizeMode: 'contain', source: { uri: "https://bilder.vinmonopolet.no/cache/200x200-0/" + item.Varenummer + "-1.jpg" } }}
-          subtitle={"Alkohol Pr. Krone: " + item.AlkoholPrKrone}
-          chevron
-          bottomDivider
-          //Links to the vinmonopolet.no site so users easily can shop their favorites
-          onPress={() => Linking.openURL(item.Vareurl).catch((err) => console.error('An error occurred', err))}
-      />
+      <Swipeout 
+      right={this.swipeoutBtns} 
+      onOpen={() => this.setState({currItem:item})}>
+        <ListItem
+            title={item.Varenavn}
+            leftAvatar={{ height: 64, width: 32, resizeMode: 'contain', source: { uri: "https://bilder.vinmonopolet.no/cache/200x200-0/" + item.Varenummer + "-1.jpg" } }}
+            subtitle={"Alkohol Pr. Krone: " + item.AlkoholPrKrone}
+            chevron
+            bottomDivider
+            //Links to the vinmonopolet.no site so users easily can shop their favorites
+            onPress={() => Linking.openURL(item.Vareurl).catch((err) => console.error('An error occurred', err))}
+            onSwpiteLeft={() => this.removeFavorite(item.Varenummer)}
+        />
+      </Swipeout>
+
   )
     render() {
         return(
@@ -52,5 +88,18 @@ class FavoriteTable extends Component{
         )
     }
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    backgroundColor: 'whitesmoke',
+    padding: 20,
+  }
+});
 
 export default FavoriteTable;
